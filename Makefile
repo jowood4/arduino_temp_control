04871901efb8fec0313cc5071cfb7d18d948a4af
Joe <jowood4@gmail.com>
2014-11-14 14:06:17 -0500

    Changed the file structure.  Moved libraries so they can be more easily compile in Arduino.  Added makefile for easy compilation

---
+++ Makefile
@@ -0,0 +1,5 @@
+BOARD_TAG    = uno
+ARDUINO_PORT = /dev/ttyACM0
+ARDUINO_LIBS = Adafruit_MCP23017 Adafruit_RGBLCDShield dht11 MAX31855 SD Wire
+
+include ../../Arduino/Arduino-Makefile/Arduino.mk
--- arduino_temp_control.ino
+++ arduino_temp_control.ino
@@ -1,7 +1,9 @@
 #include "MAX31855.h"
-#include "DHT.h"
+#include "dht11.h"
 #include "SD.h"
-#include "LiquidCrystal.h"
+#include "Adafruit_MCP23017.h"
+#include "Adafruit_RGBLCDShield.h"
+#include "Wire.h"
 
 #define DHTPIN 0     	// what pin we're connected to
 #define DHTTYPE DHT22   // DHT 22  (AM2302)
@@ -12,13 +14,12 @@
 
 #define SSR_PIN 4
 
-DHT dht(DHTPIN, DHTTYPE);
-MAX31855 thermo();
+DHT dht;
+MAX31855 thermo;
 
 void setup()
 {
 	thermo.setup(MAX31855_DATA, MAX31855_CLK, MAX31855_LAT);
-	dht.begin();
 
 	Serial.begin(9600);
 }
@@ -28,33 +29,5 @@ void loop()
 	// Wait a few seconds between measurements.
 	delay(2000);
 
-	// Reading temperature or humidity takes about 250 milliseconds!
-	// Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-	float h = dht.readHumidity();
-	// Read temperature as Celsius
-	float t = dht.readTemperature();
-	// Read temperature as Fahrenheit
-	float f = dht.readTemperature(true);
-
-	// Check if any reads failed and exit early (to try again).
-	if (isnan(h) || isnan(t) || isnan(f)) {
-	Serial.println("Failed to read from DHT sensor!");
-	return;
-	}
-
-	// Compute heat index
-	// Must send in temp in Fahrenheit!
-	float hi = dht.computeHeatIndex(f, h);
-
-	Serial.print("Humidity: "); 
-	Serial.print(h);
-	Serial.print(" %\t");
-	Serial.print("Temperature: "); 
-	Serial.print(t);
-	Serial.print(" *C ");
-	Serial.print(f);
-	Serial.print(" *F\t");
-	Serial.print("Heat index: ");
-	Serial.print(hi);
-	Serial.println(" *F");
+	dht.read22(DHTPIN);
 }
