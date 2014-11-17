ada2a3b8b2b4668de3e3dbd3a854927dd6ee5ece
Joe <jowood4@gmail.com>
2014-11-16 21:55:53 -0500

    Added state table switch statements

--- arduino_temp_control.ino
+++ arduino_temp_control.ino
@@ -14,6 +14,7 @@
 
 #define SSR_PIN 4
 
+uint8_t state = 0;
 DHT dht;
 MAX31855 thermo;
 
@@ -22,12 +23,61 @@ void setup()
 	thermo.setup(MAX31855_DATA, MAX31855_CLK, MAX31855_LAT);
 
 	Serial.begin(9600);
+
+	lcd.begin(16, 2);
+	lcd.setBacklight(WHITE);
 }
  
 void loop()
 {
-	// Wait a few seconds between measurements.
-	delay(2000);
+	switch (state) {
+	case 0:
+	        lcd.clear();
+    		lcd.setCursor(0,0);
+      		lcd.print("Final Approach");
+		lcd.setCursor(0,1);
+      		lcd.print("Composites");
+	      	
+		delay(3000);
+		state = 1;
+		break;
+	case 1:
+	      	lcd.clear();
+    		lcd.setCursor(0,0);
+      		lcd.print("Enter Max Temp");
+		lcd.setCursor(6,1);
+      		lcd.print("Deg F");
+	      	break;
+	case 2:
+	      	lcd.clear();
+    		lcd.setCursor(0,0);
+      		lcd.print("Enter Soak Time");
+		lcd.setCursor(6,1);
+      		lcd.print("Mins");
+	      	break;
+	case 3:
+	      	lcd.clear();
+    		lcd.setCursor(0,0);
+      		lcd.print("Enter Ramp Rate");
+		lcd.setCursor(6,1);
+      		lcd.print("Deg F/Min");
+	      	break;
+	case 4:
+	      	lcd.clear();
+    		lcd.setCursor(0,0);
+      		lcd.print("Enter Cool Down");
+		lcd.setCursor(6,1);
+      		lcd.print("Deg F/Min");
+	      	break;
+	case 5:
+	      	lcd.clear();
+    		lcd.setCursor(0,0);
+      		lcd.print("To Start");
+		lcd.setCursor(0,1);
+      		lcd.print("Press Enter");
+	      	break;
+	case 6:
+  	}
 
 	dht.read22(DHTPIN);
 }
